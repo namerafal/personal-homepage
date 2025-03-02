0e7de0288c6c95879ff862808fb9020e17f31f54
Rafał Olewicz <namerafal75@gmail.com>
2025-03-02 13:41:44 +0100

    Implement theme toggle functionality and update rendering structure

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Normalize } from 'styled-normalize';
+import { ThemeProvider } from 'styled-components';
+import App from './core/App/App';
+import { GlobalStyles } from './core/App/GlobalStyle';
+import { themeLight, themeDark } from './common/theme';
+
+const Root = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <React.StrictMode>
+      <ThemeProvider theme={isDarkMode ? themeDark : themeLight}>
+        <GlobalStyles />
+        <Normalize />
+        <App toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Root />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

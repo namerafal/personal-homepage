01b07c20cdf71b5841e7d1bab88ed8d5a4f68a32
Rafał Olewicz <namerafal75@gmail.com>
2025-03-05 09:53:01 +0100

    Refactor index.js to integrate Redux store and simplify theme management

--- src/index.js
+++ src/index.js
@@ -1,34 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './core/store';
 import reportWebVitals from './reportWebVitals';
-import { Normalize } from 'styled-normalize';
-import { ThemeProvider } from 'styled-components';
 import App from './core/App/App';
-import { GlobalStyles } from './core/App/GlobalStyle';
-import { themeLight, themeDark } from './common/theme';
-
-const Root = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
-  };
-
-  return (
-    <React.StrictMode>
-      <ThemeProvider theme={isDarkMode ? themeDark : themeLight}>
-        <GlobalStyles />
-        <Normalize />
-        <App toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
-      </ThemeProvider>
-    </React.StrictMode>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Root />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// to log results (for example: reportWebVitals.console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

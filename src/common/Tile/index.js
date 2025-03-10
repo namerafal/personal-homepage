0bdc2565f8e52c4aeecdb84b189c433360ba7598
Rafał Olewicz <namerafal75@gmail.com>
2025-03-10 10:31:13 +0100

    Refactor Tile component to support list rendering and enhance styling

--- src/common/Tile/index.js
+++ src/common/Tile/index.js
@@ -1,10 +1,19 @@
-import { Wrapper } from "./styled";
+import { List, ListItem } from "../../listOfSkills/styled";
+import { Content, Title, Wrapper } from "./styled";
 
-export const Tile = ({ title, content }) => {
-    return (
-        <Wrapper>
-            <h2>{title}</h2>
-            <p>{content}</p>
-        </Wrapper>
-    );
-}
+export const TileList = ({ title, content }) => (
+    <Wrapper>
+        <Title>{title}</Title>
+        <Content>
+            {Array.isArray(content) ? (
+                    <List>
+                      {content.map((item, index) => (
+                        <ListItem key={index}>{item}</ListItem>
+                      ))}
+                    </List>
+                  ) : (
+                    content
+                  )}
+        </Content>
+    </Wrapper>
+);
--- src/common/Tile/styled.js
+++ src/common/Tile/styled.js
@@ -1,11 +1,40 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    padding: 20px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    margin: 0px 10px 20px 10px;
+    padding: 32px;
+    margin: 0px;
     width: 100%;
+    box-shadow: ${({ theme }) => theme.boxShadow};
+    border-radius: ${({ theme }) => theme.borderRadiusSmall};
 
     background: ${({ theme }) => theme.colors.tileBackground};
+`;
+
+export const Title = styled.h2`
+    font-size: 30px;
+    font-weight: 900;
+    margin: 0px;
+    padding-bottom: 16px;
+    color: ${({ theme }) => theme.colors.textPrimary};
+    border-bottom: 1px solid ${({ theme }) => theme.colors.headerLine};    
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        font-size: 18px;
+        padding-bottom: 12px;
+    }
+`;
+
+export const Content = styled.div`
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 1.4;
+    padding: 0px;
+    margin-top: 32px;
+    color: ${({ theme }) => theme.colors.textSecondary};
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        font-size: 14px;
+        line-height: 1;
+        margin-top: 12px;
+    }
 `;

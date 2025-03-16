0fd1940fb2ab60c1bdd29e3b8a3c491e33b951e3
Rafał Olewicz <namerafal75@gmail.com>
2025-03-16 14:14:58 +0100

    Refactor MainInformation component for improved code readability and structure

--- src/features/personalHomepage/PersonalHomepage/MainInformation/index.js
+++ src/features/personalHomepage/PersonalHomepage/MainInformation/index.js
@@ -5,20 +5,18 @@ import { ButtonIcon, Name, Picture, ShortDescription, StyledButtonLink, ThisIs, 
 export const MainInformation = () => {
     return (
         <Wrapper>
-        <Picture src={rafalPicture} alt="Rafal picture" />
-        <div>
-            <ThisIs>This is</ThisIs>
-            <Name>Rafał Olewicz</Name>
-            <ShortDescription>
-                {/* <Icon src={faceIcon} alt="Face icon" />
-                <Icon src={laptopIcon} alt="Laptop icon" /> */}
-                🧑🏻💻I’m a passionate Frontend Developer in love with React, currently looking for new job opportunities.
-            </ShortDescription>
-            <StyledButtonLink href={`mailto:${email}`} title={email}>
-                <ButtonIcon/>
-                Hire me
-            </StyledButtonLink>
-        </div>
-    </Wrapper>
+            <Picture src={rafalPicture} alt="Rafal picture" />
+            <div>
+                <ThisIs>This is</ThisIs>
+                <Name>Rafał Olewicz</Name>
+                <ShortDescription>
+                    🧑🏻💻I’m a passionate Frontend Developer in love with React, currently looking for new job opportunities.
+                </ShortDescription>
+                <StyledButtonLink href={`mailto:${email}`} title={email}>
+                    <ButtonIcon />
+                    Hire me
+                </StyledButtonLink>
+            </div>
+        </Wrapper>
     );
 };

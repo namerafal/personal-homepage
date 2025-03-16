a55367ee88ce8dd5fb92d8268c3ab84eb55b7d10
Rafał Olewicz <namerafal75@gmail.com>
2025-03-16 14:14:35 +0100

    Refactor Header styled components for improved layout and typography

--- src/features/personalHomepage/PersonalHomepage/Header/styled.js
+++ src/features/personalHomepage/PersonalHomepage/Header/styled.js
@@ -6,20 +6,23 @@ export const TitleWrapper = styled.header`
 `;
 
 export const Icon = styled.div`
-  width: 48px;
-  height: 48px;
-  margin: 0 auto;
+  width: 40px;
+  height: 40px;
+  margin-bottom: 12px;
   color: ${({ theme }) => theme.colors.primary};
 `;
 
 export const Title = styled.h2`
   font-size: 30px;
   line-height: 1;
+  font-weight: 900;
   color: ${({theme}) => theme.colors.textPrimary};
-  margin: 8px 0;
+  margin: 0;
 `;
 
 export const Subtitle = styled.p`
-  font-size: 16px;
-  color: ${({ theme }) => theme.colors.textSecondary};
+  font-size: 20px;
+  line-height: 2;
+  margin: 8px 0px 0px;
+  color: ${({ theme }) => theme.colors.textPrimary};
 `;

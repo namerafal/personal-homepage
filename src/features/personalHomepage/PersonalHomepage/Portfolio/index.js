7122d0f68609f6898e846ec1d7290c0420b13bfb
Rafał Olewicz <namerafal75@gmail.com>
2025-03-18 16:17:24 +0100

    Remove commented-out GitHubIcon import from Portfolio component

--- src/features/personalHomepage/PersonalHomepage/Portfolio/index.js
+++ src/features/personalHomepage/PersonalHomepage/Portfolio/index.js
@@ -8,7 +8,6 @@ import {
 import { RepoTile, RepoList } from './styled';
 import { RepoLinks } from './Links';
 import { Loading } from '../../../../common/Loading';
-// import { ReactComponent as GitHubIcon } from '../Footer/SocialIcons/icons/github.svg';
 
 const Portfolio = ({ username = 'namerafal' }) => {
   const dispatch = useDispatch();
@@ -29,7 +28,6 @@ const Portfolio = ({ username = 'namerafal' }) => {
 
   return (
     <>
-    
       <RepoList>
         {repositories && repositories.length > 0 ? (
           repositories.map(repo => (

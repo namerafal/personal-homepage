6ea935a5c6a47115369bf7452e02af4dc29f0006
Rafał Olewicz <namerafal75@gmail.com>
2025-03-22 21:57:51 +0100

    Enhance ListOfSkills component with improved empty state messaging and responsive layout adjustments

--- src/listOfSkills/index.js
+++ src/listOfSkills/index.js
@@ -1,15 +1,18 @@
 import { List, ListItem } from "./styled";
 
-export const ListOfSkills = ({content}) => (
-    <>
-        {Array.isArray(content) ? (
-        <List>
-          {content.map((item, index) => (
-            <ListItem key={index}>{item}</ListItem>
-          ))}
-        </List>
-      ) : (
-        "🤷🏻‍♂️ Something went wrong."
-      )}
-    </>
+export const ListOfSkills = ({ content }) => (
+  <>
+    {Array.isArray(content) ? (
+      <List>
+        {content.length > 0 ? (content.map((item, index) => (
+          <ListItem key={index}>{item}</ListItem>
+        ))
+        ) : (
+          <p>There is a lot for sure, <br/> but I can't show all of them right now. 🙄</p>
+        )}
+      </List>
+    ) : (
+      <p>🤷🏻‍♂️ Something went wrong. <br/> Let me know about this situation, please.</p>
+    )}
+  </>
 );
--- src/listOfSkills/styled.js
+++ src/listOfSkills/styled.js
@@ -1,21 +1,27 @@
 import styled from 'styled-components';
 
-export const List = styled.ul`
+export const List = styled.ul`   
     list-style-type: none;
     padding: 0;
     margin: 0;
     display: grid;
     align-items: center;
-    grid-template-columns: repeat(auto-fill, minmax(314px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(clamp(230px, 30%, 314px), 1fr));
+    grid-column-gap: 12px;
     grid-row-gap: 8px;
     
     @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {        
         grid-template-columns: repeat(1, minmax(238px, 1fr));
     }
+    
+        & p { 
+            grid-column: 1 / -1;
+            text-align: center;
+        }
 `;
 
-export const ListItem = styled.li`
-    max-height: fit-content;
+export const ListItem = styled.li`   
+    max-width: fit-content;
     display: flex;
     align-items: center;
     position: relative;

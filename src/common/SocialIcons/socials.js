5c138df66828da93aa879558bbcd7b7d5d63e25c
Rafał Olewicz <namerafal75@gmail.com>
2025-11-21 17:45:34 +0100

    Refactor social icons and update CV links
    - Updated GitHub and Instagram SVG icons for improved readability and structure.
    - Added Polish and English CV icons to the social icons list.
    - Updated CV links to point to the correct documents for both Polish and English versions.

--- src/common/SocialIcons/socials.js
+++ src/common/SocialIcons/socials.js
@@ -3,7 +3,8 @@ import { ReactComponent as GithubIcon } from "./icons/github.svg";
 import { ReactComponent as LinkedInIcon } from "./icons/linkedIN.svg";
 // import { ReactComponent as InstagramIcon } from "./icons/instagram.svg";
 import { ReactComponent as FacebookIcon } from "./icons/facebook.svg";
-import { ReactComponent as CvIcon } from "./icons/cvicon.svg";
+import { ReactComponent as CvIconPl } from "./icons/cviconpl.svg";
+import { ReactComponent as CvIconEn } from "./icons/cviconen.svg";
 
 export const socials = [
     {
@@ -26,9 +27,16 @@ export const socials = [
         url: "https://www.facebook.com/zrobmystrone",
         Icon: styleIcon(FacebookIcon),
     },
+
     {
-        name: "CV",
-        url: "https://drive.google.com/file/d/1UWsHPEe9ztSQ372zONO7hhPELiB1rlQI/view?usp=sharing",
-        Icon: styleIcon(CvIcon),
+        name: "CV-PL",
+        url: "https://drive.google.com/file/d/1i5S7XD5xwsFOWd5vanfmu1YcqgWhPego/view?usp=sharing",
+        Icon: styleIcon(CvIconPl),
+    },
+
+    {
+        name: "CV-EN",
+        url: "https://drive.google.com/file/d/1XaOj_KrQtELJ8HiV31T_t0Kp1sdCMJRj/view?usp=sharing",
+        Icon: styleIcon(CvIconEn),
     },
 ];
